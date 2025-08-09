d99f86320dc65f6be6280ba1ee7588d3c0b15ba4
arafat-jahan <ajh.official15@gmail.com>
2025-08-10 00:16:45 +0600

    cartscreen updated

--- lib/repository/screens/cart/cartscreen.dart
+++ lib/repository/screens/cart/cartscreen.dart
@@ -1,188 +1,149 @@
+import 'package:blinkit_app/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
-import '../../widgets/uihelper.dart';
 
 class CartScreen extends StatelessWidget {
-  final TextEditingController searchController = TextEditingController();
-
+  TextEditingController searchContorller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          // 🔽 নিচের অংশের মূল কনটেন্ট (scrollable বা fixed)
-          Column(
+          SizedBox(
+            height: 40,
+          ),
+          Stack(
             children: [
-              SizedBox(height: 40),
-
               Container(
-                height: 190,
+                height: 200,
                 width: double.infinity,
+                color: Color(0XFFF7CB45),
                 child: Column(
                   children: [
-                    Stack(
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
                       children: [
-                        Container(
-                          height: 190,
-                          width: double.infinity,
-                          color: Color(0XFFF7CB45),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 30),
-                              Padding(
-                                padding: EdgeInsets.only(left: 20),
-                                child: UiHelper.CustomText(
-                                  text: "Blinkit in",
-                                  color: Color(0XFF000000),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15,
-                                  fontfamily: "bold",
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(left: 20),
-                                child: UiHelper.CustomText(
-                                  text: "16 minutes",
-                                  color: Color(0XFF000000),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  fontfamily: "bold",
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(left: 20),
-                                child: Row(
-                                  children: [
-                                    UiHelper.CustomText(
-                                      text: "HOME ",
-                                      color: Color(0XFF000000),
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 14,
-                                    ),
-                                    UiHelper.CustomText(
-                                      text: "- Sujal Dave, Ratanada, Jodhpur (Raj)",
-                                      color: Color(0XFF000000),
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 14,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
+                        SizedBox(
+                          width: 10,
                         ),
-                        Positioned(
-                          right: 20,
-                          bottom: 100,
-                          child: CircleAvatar(
-                            radius: 15,
-                            backgroundColor: Colors.white,
-                            child: Icon(
-                              Icons.person,
-                              color: Colors.black,
-                              size: 20,
-                            ),
-                          ),
+                        UiHelper.CustomText(
+                            text: "Blinkit in",
+                            color: Color(0XFF000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            fontfamily: "bold"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 10,
                         ),
-                        Positioned(
-                          bottom: 30,
-                          left: 20,
-                          child: UiHelper.CustomTextField(
-                            controller: searchController,
-                          ),
+                        UiHelper.CustomText(
+                            text: " 16 minutes",
+                            color: Color(0XFF000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            fontfamily: "bold")
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 10,
                         ),
+                        UiHelper.CustomText(
+                            text: " Home",
+                            color: Color(0XFF000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12),
+                        UiHelper.CustomText(
+                            text: " -Arafat Jahan ,Dhaka Bangladesh",
+                            color: Color(0XFF000000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12)
                       ],
                     ),
                   ],
                 ),
               ),
-
-              SizedBox(height: 20),
-              UiHelper.CustomImage(img: "cart.png"),
-              SizedBox(height: 20),
-              UiHelper.CustomText(
-                text: "Reordering will be easy",
-                color: Color(0XFF000000),
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                fontfamily: "bold",
-              ),
-              UiHelper.CustomText(
-                text: "Items you order will show up here so you can buy",
-                color: Color(0XFF000000),
-                fontWeight: FontWeight.bold,
-                fontSize: 12,
-              ),
-              UiHelper.CustomText(
-                text: "them again easily.",
-                color: Color(0XFF000000),
-                fontWeight: FontWeight.bold,
-                fontSize: 12,
-              ),
-
-              SizedBox(height: 30),
-
-              Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: UiHelper.CustomText(
-                  text: "Bestsellers",
-                  color: Color(0XFF000000),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  fontfamily: "bold",
-                ),
-              ),
-
-              SizedBox(height: 10),
-
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Row(
-                    children: [
-                      Stack(
-                        children: [
-                          UiHelper.CustomImage(img: "milk.png"),
-                          Positioned(
-                            top: 95,
-                            left: 65,
-                            child: UiHelper.CustomButton(() {}),
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 15),
-                      Stack(
-                        children: [
-                          UiHelper.CustomImage(img: "potato.png"),
-                          Positioned(
-                            top: 95,
-                            left: 65,
-                            child: UiHelper.CustomButton(() {}),
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 15),
-                      Stack(
-                        children: [
-                          UiHelper.CustomImage(img: "tomato.png"),
-                          Positioned(
-                            top: 95,
-                            left: 65,
-                            child: UiHelper.CustomButton(() {}),
-                          ),
-                        ],
-                      ),
-                    ],
+              Positioned(
+                right: 10,
+                bottom: 80,
+                child: CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                    size: 20,
                   ),
                 ),
               ),
-              SizedBox(height: 20),
+              Positioned(
+                  bottom: 20,
+                  left: 20,
+                  child: UiHelper.CustomTextField(
+                    controller: searchContorller,
+                  )),
             ],
           ),
+          SizedBox(
+            height: 20,
+          ),
+          UiHelper.CustomImage(img: "cart.png"),
+          SizedBox(
+            height: 30,
+          ),
+          UiHelper.CustomText(
+              text: "Reordering will be easy",
+              color: Color(0XFF000000),
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              fontfamily: "Bold"),
+          SizedBox(
+            height: 5,
+          ),
+          UiHelper.CustomText(
+              text: "Items you order will show up here so you can buy",
+              color: Color(0XFF000000),
+              fontWeight: FontWeight.normal,
+              fontSize: 10),
+          UiHelper.CustomText(
+              text: "them again easily",
+              color: Color(0XFF000000),
+              fontWeight: FontWeight.normal,
+              fontSize: 10),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              UiHelper.CustomText(
+                  text: "Bestsellers",
+                  color: Color(0XFF000000),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            ],
+          ),
+          SizedBox(height: 20,),
+         Row(
+           children: [
+             SizedBox(height: 20,),
+Stack(
+  children: [
+    UiHelper.CustomImage(img: "milk.png"),
+    
+    Center(child: Padding(padding:EdgeInsets.only(top: 98))),
 
-          // 🔼 Stack-এর উপরে overlay করতে চাইলে এখানে আলাদা Positioned widget রাখতে পারো
-          // উদাহরণ: App-wide floating button, banner, loading indicator ইত্যাদি
+
+  ],
+)           ],
+         )
         ],
       ),
     );
--- lib/repository/widgets/uihelper.dart
+++ lib/repository/widgets/uihelper.dart
@@ -20,28 +20,33 @@ class UiHelper {
           color: color),
     );
   }
-  static CustomTextField({required TextEditingController controller}){
+
+  //here creating textfld
+
+static CustomTextField({required TextEditingController controller, }){
     return Container(
-      height: 40,
+      height: 37,
       width: 360,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-          border: Border.all(
-              color: Color(0XFFC5C5C5)
-          )
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        border: Border.all(
+          color: Color(0XFFC5C5C5),
+        ),
       ),
       child: TextField(
         controller: controller,
         decoration: InputDecoration(
-            hintText: "Search 'ice-cream'",
-            prefixIcon: Image.asset("assets/images/search.png"),
-            suffixIcon: Image.asset("assets/images/mic 1.png"),
-            border: InputBorder.none
-        ),
+          hintText: "Search 'ice-cream'",
+          prefixIcon: Image.asset("assets/images/search.png"),
+          suffixIcon: Image.asset("assets/images/mic 1.png"),
+          border: InputBorder.none,
+
+        )
       ),
     );
-  }
+}
+
 
   static CustomButton(VoidCallback callback){
     return Container(
@@ -57,4 +62,5 @@ class UiHelper {
       child: Center(child: Text("Add",style: TextStyle(fontSize: 8,color: Color(0XFF27AF34)),),),
     );
   }
-}
+
+
